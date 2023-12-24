462b78544828b9c4b6ba382c1b0201cf1bf0e2f3
David Canales <canalesparradavid@gmail.com>
2023-12-24 13:33:57 +0100

    Refactorizado para funcionar con cualquier nivel de profundidad

--- src/Components/GaltonMachine.cpp
+++ src/Components/GaltonMachine.cpp
@@ -1,30 +1,38 @@
 #include "GaltonMachine.h"
+#include "StandardNail.h"
 #include <stdlib.h>
+#include <queue>
+
+using namespace std;
 
 GaltonMachine::GaltonMachine(int depth) {
     Depth = depth;
     Box = calloc(depth, sizeof(int));
 }
 
+void createBranches(NailBinaryTree *r, queue<NailBinaryTree*> *q, int maxDepth, int actualDepth) {
+    if (actualDepth == maxDepth) return;
+
+    if (!q->empty()) {
+        r->Left = q->front();
+        q->pop();
+    }
+    else {
+        NailBinaryTree *newLeftNode = new NailBinaryTree(new StandardNail(1));
+        r->Left = newLeftNode;
+        createBranches(newLeftNode, q, maxDepth, actualDepth + 1);
+    }
+    NailBinaryTree *newRightNode = new NailBinaryTree(new StandardNail(1));
+    r->Right = newRightNode;
+    q->push(newRightNode);
+    createBranches(newRightNode, q, maxDepth, actualDepth + 1);
+}
+
 void GaltonMachine::SetupNails() {
-    // TODO: esto es provisional
-    NailBinaryTree *n1 = new NailBinaryTree(nullptr);
-    NailBinaryTree *n2 = new NailBinaryTree(nullptr);
-    NailBinaryTree *n3 = new NailBinaryTree(nullptr);
-    NailBinaryTree *n4 = new NailBinaryTree(nullptr);
-    NailBinaryTree *n5 = new NailBinaryTree(nullptr);
-    NailBinaryTree *n6 = new NailBinaryTree(nullptr);
+    NailTree = new NailBinaryTree(new StandardNail(1));
 
-    n1->Left = n2;
-    n1->Right = n3;
-
-    n2->Left = n4;
-    n2->Right = n5;
-
-    n3->Left = n5;
-    n3->Right = n6;
-
-    NailTree = n1;
+    queue<NailBinaryTree*> q;
+    createBranches(NailTree, &q, Depth, 1);
 }
 
 int GaltonMachine::GetBallsInBox(int boxNumber) {
